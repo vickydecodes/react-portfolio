63039a900839225212b21813020b11673c1a290f
vicky-at-web <darklegendvs@gmail.com>
2024-09-01 16:34:29 +0530

    btns cleanup

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -40,15 +40,15 @@ export const Home = () => {
                 <p className="mb-1x">{introdata.description}</p>
                 <div className="intro_btn-action pb-5">
                   <Link to="/portfolio" className="text_2">
-                    <div id="button_p" className="ac_btn btn">
+                    <div id="button_p" className="ac_btn btn mb-3">
                       My Portfolio
                       <div className="ring one"></div>
                       <div className="ring two"></div>
                       <div className="ring three"></div>
                     </div>
                   </Link>
-                  <Link to="/contact">
-                    <div id="button_h" className="ac_btn btn">
+                  <Link to="/contact" >
+                    <div id="button_h" className="ac_btn btn mb-3">
                       Contact Me
                       <div className="ring one"></div>
                       <div className="ring two"></div>
