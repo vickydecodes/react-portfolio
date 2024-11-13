7b45b84ad26588155602b2dc7c2028c32e07ddc1
vickydecodes <darklegendvs@gmail.com>
2024-11-13 17:19:37 +0530

    Update Contact.jsx

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -68,7 +68,7 @@ export const ContactUs = () => {
       <Container>
         <Helmet>
           <meta charSet="utf-8" />
-          <title>{meta.title} | Contact</title>
+          <title>Contact | {meta.title}  </title>
           <meta name="description" content={meta.description} />
         </Helmet>
         <Row className="mb-5 mt-3 pt-md-3">
