89f10ed161814859825469a712de01d89bcf5d65
vickydecodes <darklegendvs@gmail.com>
2025-01-02 11:59:51 +0530

    Update contentOptions.jsx

--- src/utils/contentOptions.jsx
+++ src/utils/contentOptions.jsx
@@ -1,164 +1,182 @@
 const logotext = "Vicky";
 const meta = {
-    title: "Vignesh Karthikeyan",
-    description: "I’m Vicky, a MERN stack devloper,currently working in Chennai",
+  title: "Vignesh Karthikeyan",
+  description: "I’m Vicky, a MERN stack devloper,currently working in Chennai",
 };
 
 const introdata = {
-    title: "I’m Vicky",
-    animated: {
-        first: "Crafting innovative code",
-        second: "Building dynamic websites",
-        third: "Developing robust web apps",
-    },
-    description: "Hi, I'm Vignesh, a passionate Full Stack Developer and BCA student, graduating in 2025. I've built diverse projects ranging from e-commerce platforms to machine learning applications. With a love for modern tech stacks like React and Firebase, I thrive on turning ideas into impactful web applications. Let's create something amazing together!",
-    your_img_url: "https://res.cloudinary.com/dyrcq4l1g/image/upload/v1725174991/portfolio/tcq9pgmgafmivufaoynk.png",
+  title: "I’m Vicky",
+  animated: {
+    first: "Crafting innovative code",
+    second: "Building dynamic websites",
+    third: "Developing robust web apps",
+  },
+  description:
+    "Hi, I'm Vignesh, a passionate Full Stack Developer and BCA student, graduating in 2025. I've built diverse projects ranging from e-commerce platforms to machine learning applications. With a love for modern tech stacks like React and Firebase, I thrive on turning ideas into impactful web applications. Let's create something amazing together!",
+  your_img_url:
+    "https://res.cloudinary.com/dyrcq4l1g/image/upload/v1725174991/portfolio/tcq9pgmgafmivufaoynk.png",
 };
 
 const dataabout = {
-    title: "A bit about myself",
-    aboutme: "Hi, I'm Vignesh, a passionate Full Stack Developer and BCA student, graduating in 2025. I've built diverse projects ranging from e-commerce platforms to machine learning applications. With a love for modern tech stacks like React and Firebase, I thrive on turning ideas into impactful web applications. Let's create something amazing together!",
+  title: "A bit about myself",
+  aboutme:
+    "Hi, I'm Vignesh, a passionate Full Stack Developer and BCA student, graduating in 2025. I've built diverse projects ranging from e-commerce platforms to machine learning applications. With a love for modern tech stacks like React and Firebase, I thrive on turning ideas into impactful web applications. Let's create something amazing together!",
 };
 const worktimeline = [
-    {
-        jobtitle: "Full Stack Web Developer",
-        where: "Freelance",
-        date: "2023 - present",
-    },
-    {
-        jobtitle: "Web Developer",
-        where: "Innotech Titans",
-        date: "2024 - present",
-    }
+  {
+    jobtitle: "Full Stack Web Developer",
+    where: "Freelance",
+    date: "2023 - present",
+  },
+  {
+    jobtitle: "Web Developer",
+    where: "Innotech Titans",
+    date: "2024 - present",
+  },
 ];
 
-const skills = [{
+const skills = [
+  {
     name: "Python",
     value: 90,
-},
-{
+  },
+  {
     name: "Node JS",
     value: 85,
-},
-{
+  },
+  {
     name: "Javascript",
     value: 80,
-},
-{
+  },
+  {
     name: "React",
     value: 85,
-},
-{
-    name: 'Express JS',
-    value: 80
-},
-{
-    name: 'Mongo DB',
-    value: 75
-},
-{
+  },
+  {
+    name: "Express JS",
+    value: 80,
+  },
+  {
+    name: "Mongo DB",
+    value: 75,
+  },
+  {
     name: "Git & GitHub",
     value: 85,
-},
+  },
 ];
 
 const services = [
-    {
-        title: "Website Development",
-        description: "I specialize in creating user-focused applications, leveraging my skills in both front-end and back-end technologies. I use React and Bootstrap for the front end, and Firebase and MongoDB for the back end, to build efficient, scalable, and visually appealing solutions.",
-    },
-    {
-        title: "Web Apps",
-        description: "I design and build user-friendly applications. My focus is on ensuring efficiency, scalability, and visual appeal. I strive to create solutions that meet both user needs and project goals.",
-    }
+  {
+    title: "Website Development",
+    description:
+      "I specialize in creating user-focused applications, leveraging my skills in both front-end and back-end technologies. I use React and Bootstrap for the front end, and Firebase and MongoDB for the back end, to build efficient, scalable, and visually appealing solutions.",
+  },
+  {
+    title: "Web Apps",
+    description:
+      "I design and build user-friendly applications. My focus is on ensuring efficiency, scalability, and visual appeal. I strive to create solutions that meet both user needs and project goals.",
+  },
 ];
 
 const education = [
-    {
-        title: "Bachelor Of Computer Applications",
-        duration: '2022 - 2025',
-        school: 'Gurunanak College, Velachery',
-        description: "Learned various technologies Web Technologies, Programming Languages, Data Structures, Mobile App Development, OS, Databases",
-        cgpa: 'CGPA: 7.85 out of 9.5'
-    },
-    {
-        title: "11th - 12th",
-        duration: '2021 - 2022',
-        school: 'Holy Family Convent Matriculation Hr Sec School, Keelkatalai',
-        description: "Secured 72% in HSC Examination.",
-    },
-    {
-        title: "1st - 10th",
-        duration: '2010 - 2020',
-        school: 'Good Shepherd Matriculation Hr Sec School, Old Pallavaram',
-        description: "Secured 98.7% in SSLC Examination.",
-    }
-]
+  {
+    title: "Bachelor Of Computer Applications",
+    duration: "2022 - 2025",
+    school: "Gurunanak College, Velachery",
+    description:
+      "Learned various technologies Web Technologies, Programming Languages, Data Structures, Mobile App Development, OS, Databases",
+    cgpa: "CGPA: 7.85 out of 9.5",
+  },
+  {
+    title: "11th - 12th",
+    duration: "2021 - 2022",
+    school: "Holy Family Convent Matriculation Hr Sec School, Keelkatalai",
+    description: "Secured 72% in HSC Examination.",
+  },
+  {
+    title: "1st - 10th",
+    duration: "2010 - 2020",
+    school: "Good Shepherd Matriculation Hr Sec School, Old Pallavaram",
+    description: "Secured 98.7% in SSLC Examination.",
+  },
+];
 
-const dataportfolio = [{
+const dataportfolio = [
+  {
     img: "https://res.cloudinary.com/dyrcq4l1g/image/upload/v1725178942/portfolio/project/tp5dt5umbitbxhm6fbkv.png",
     description: "The Tribute to Virat Kohli",
     link: "https://vickydecodes.github.io/viratkohli18/viraat2.html",
-},
-{
+  },
+  {
     img: "https://res.cloudinary.com/dyrcq4l1g/image/upload/v1725179388/portfolio/project/uul4yhztqhhfkmqdh8k6.png",
     description: "Yelpcamp",
     link: "https://yelpcamp-z482.onrender.com",
-},
-{
+  },
+  {
     img: "https://res.cloudinary.com/dyrcq4l1g/image/upload/v1725179795/portfolio/project/vf4zc2avnffpam9n1dom.png",
     description: "React Todo App",
     link: "https://react-todos-0d4p.onrender.com/",
-},
-{
+  },
+  {
     img: "https://res.cloudinary.com/dyrcq4l1g/image/upload/v1725180026/portfolio/project/kbkkkn2gcgioedonqbfr.png",
     description: "TV show finder app",
     link: "https://vickydecodes.github.io/tvshow/tv.html",
-},
+  },
 
-{
+  {
     img: "https://res.cloudinary.com/dyrcq4l1g/image/upload/v1725180141/portfolio/project/urzh9t1psdht5pvhjbey.png",
     description: "Snake game",
     link: "https://vickydecodes.github.io/snakegame/snake.html",
-},
-{
+  },
+  {
     img: "https://res.cloudinary.com/dyrcq4l1g/image/upload/v1725180554/portfolio/project/f2i4gt0m6nvcspcocq3c.png",
     description: "Color cubes",
     link: "https://reactapp-bdo0.onrender.com/",
-},
-{
+  },
+  {
     img: "https://res.cloudinary.com/dyrcq4l1g/image/upload/v1725289558/portfolio/project/k6nggfjaza3sbum1cntd.png",
     description: "Arulmigu Sree Balamuneeswaran Temple Website",
     link: "https://www.arulmigusreebalamuneeswarantemple.org",
-}
-
+  },
+  {
+    img: "https://res.cloudinary.com/dyrcq4l1g/image/upload/v1735799142/portfolio/project/byqkv46hkwfdzexqocju.png",
+    description: "Send Anywhere Clone",
+    link: "https://sendanywhere-frontend.onrender.com/",
+  },
+  {
+    img: "https://res.cloudinary.com/dyrcq4l1g/image/upload/v1735799316/portfolio/project/wljttywimmptifypsuvp.png",
+    description: "ChatGPT Clone",
+    link: "https://openai-i68y.onrender.com",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "workofvicky@gmail.com",
-    YOUR_FONE: "+91 63851 38282",
-    description: "I'm Vignesh, a Full Stack Developer with a passion for crafting user-centric web applications. I specialize in creating efficient, scalable, and visually appealing solutions. Driven by a commitment to excellence, I enjoy turning innovative ideas into impactful digital experiences. ",
-    YOUR_SERVICE_ID: "service_hu8ywwe",
-    YOUR_TEMPLATE_ID: "template_5ct48vj",
-    YOUR_USER_ID: "uV8HH3oXRJexv7SOP",
+  YOUR_EMAIL: "workofvicky@gmail.com",
+  YOUR_FONE: "+91 63851 38282",
+  description:
+    "I'm Vignesh, a Full Stack Developer with a passion for crafting user-centric web applications. I specialize in creating efficient, scalable, and visually appealing solutions. Driven by a commitment to excellence, I enjoy turning innovative ideas into impactful digital experiences. ",
+  YOUR_SERVICE_ID: "service_hu8ywwe",
+  YOUR_TEMPLATE_ID: "template_5ct48vj",
+  YOUR_USER_ID: "uV8HH3oXRJexv7SOP",
 };
 
 const socialprofils = {
-    github: "https://github.com/vickydecodes",
-    facebook: "https://www.facebook.com/vickysde/",
-    linkedin: "https://www.linkedin.com/in/vickyatln26/",
-    twitter: "https://x.com/vickytweetzz",
+  github: "https://github.com/vickydecodes",
+  facebook: "https://www.facebook.com/vickysde/",
+  linkedin: "https://www.linkedin.com/in/vickyatln26/",
+  twitter: "https://x.com/vickytweetzz",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
-    education
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+  education,
 };
