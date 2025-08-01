c63dfd833169fec44f77154badba702922bcfcda
vicky <144013983+vickydecodes@users.noreply.github.com>
2025-08-01 13:23:57 +0530

    Update contentOptions.jsx

--- src/utils/contentOptions.jsx
+++ src/utils/contentOptions.jsx
@@ -262,9 +262,9 @@ const contactConfig = {
   YOUR_FONE: "+91 63851 38282",
   description:
     "I'm Vignesh, a Full Stack Developer and the mind behind Cookie, with a passion for building user-centric digital experiences. I specialize in crafting scalable, efficient, and visually engaging web solutions using modern tech stacks. Fueled by innovation and a drive for excellence, I turn bold ideas into impactful applications that deliver real value.",
-  YOUR_SERVICE_ID: "service_hu8ywwe",
-  YOUR_TEMPLATE_ID: "template_5ct48vj",
-  YOUR_USER_ID: "uV8HH3oXRJexv7SOP",
+  YOUR_SERVICE_ID: "service_r97lzna",
+  YOUR_TEMPLATE_ID: "template_2hu5age",
+  YOUR_USER_ID: "tZ6uCr0jgGzlfsL97",
 };
 
 const socialprofils = {
